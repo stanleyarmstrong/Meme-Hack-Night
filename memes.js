ca478b505a7600d55869089b492ef00a5db51b19
stonewallstan <stanleyarmstrong31@gmail.com>
2016-09-09 20:19:13 -0700

    The Javascript Adds the motion of the Images

---
+++ memes.js
@@ -0,0 +1,50 @@
+var k = 0;
+
+function flip(){
+  var j = document.getElementById("stolenMemes");
+  k+=180;
+  j.style.transform ="rotate(" + k + "deg)";
+  j.style.transitionDuration ="0.5s";
+
+}
+function flip2(){
+  var j = document.getElementById("stolenMemes1");
+  k+=180;
+  j.style.transform ="rotate(" + k + "deg)";
+  j.style.transitionDuration ="0.5s";
+
+}
+function flip3(){
+  var j = document.getElementById("stolenMemes2");
+  k+=180;
+  j.style.transform ="rotate(" + k + "deg)";
+  j.style.transitionDuration ="0.5s";
+
+}
+function flip4(){
+  var j = document.getElementById("stolenMemes3");
+  k+=180;
+  j.style.transform ="rotate(" + k + "deg)";
+  j.style.transitionDuration ="0.5s";
+
+}
+function flip5(){
+  var j = document.getElementById("stolenMemes4");
+  k+=180;
+  j.style.transform ="rotate(" + k + "deg)";
+  j.style.transitionDuration ="0.5s";
+
+}
+function flip6(){
+  var j = document.getElementById("stolenMemes5");
+  k+=180;
+  j.style.transform ="rotate(" + k + "deg)";
+  j.style.transitionDuration ="0.5s";
+
+}
+flip2();
+flip();
+flip3();
+flip4();
+flip5();
+flip6();
