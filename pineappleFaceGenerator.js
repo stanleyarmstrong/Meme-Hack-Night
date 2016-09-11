a54ea4dfc048735d181c16484b97979b39238404
Stanley Armstrong <stanleyarmstrong31@gmail.com>
2016-09-10 22:07:09 -0700

    Added some logs to the console

--- pineappleFaceGenerator.js
+++ pineappleFaceGenerator.js
@@ -1,3 +1,6 @@
 function pineappleFace(){
+  console.log("This button has been clicked.");
+  console.log("Preparing the user for their Pineapple Side.");
   
+  console.log("The user has found their Pineapple Side");
 }
